7d3221c1f225d78e0486a4661b80584f559df1e0
Mark Kenneth Ranosa <Ken@marks-mbp.lan>
2014-10-01 18:07:40 +0800

    Meow

--- www/app/main/main.controller.js
+++ www/app/main/main.controller.js
@@ -3,19 +3,19 @@
 angular.module('seedApp')
 	.controller('MainCtrl', function($scope) {
 		$scope.awesomeThings = [{
-			name: 'TODO: Development Tools',
-			info: 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
+			name: 'Chocolate muffin',
+			info: '...'
 		}, {
-			name: 'TODO: Smart Build System',
-			info: 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
+			name: 'Donut bear claw',
+			info: '...'
 		}, {
-			name: 'Modular Structure',
-			info: 'Best practice client and server structures allow for more code reusability and maximum scalability'
+			name: 'Liquorice gummies',
+			info: '...'
 		}, {
-			name: 'TODO: Optimized Build',
-			info: 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
+			name: 'Apple pie oat cake',
+			info: '...'
 		}, {
-			name: 'TODO: Deployment Ready',
-			info: 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
+			name: 'Caramels jujubes',
+			info: '...'
 		}]
 	});
