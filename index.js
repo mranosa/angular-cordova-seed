f93cb7adda9c3b51c77242c3d425534feecdff15
Mark Kenneth Ranosa <Ken@marks-mbp.lan>
2014-10-09 10:58:05 +0800

    Removed ecstatic and initial way of serving up seed

--- index.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var serveStaticFiles = require('ecstatic')({ root: __dirname + '/www' });
-var port = process.env.PORT || 8000;
-
-http.createServer(function (req, res) {
-    serveStaticFiles(req, res);
-}).listen(port);
-
-console.log('Listening on http://localhost:%d', port);
